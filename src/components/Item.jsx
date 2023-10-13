293c518f9e7d9cf2892b6087c4542a3cc805b05e
Mohammad javad Jafar Abady <mjafary822@gmail.com>
2023-10-13 18:20:51 +0330

    fix flipped items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,6 @@
 const Item = ({ index, image, isShow, onClick,reset }) => {
   return (
-    <div className="memory-game__item" onClick={onClick}>
+    <div className={`memory-game__item ${isShow ? 'flipped' : ''}`} onClick={onClick}>
       {isShow ? <img src={image} /> : index}
     </div>
 
